88f2c490cd881840002d59f0aa4f8fb82b906f28
Wolf McNally <wolf@wolfmcnally.com>
2024-01-31 03:39:35 -0800

    Updated libwally to 0.9.0

--- build.sh
+++ build.sh
@@ -10,57 +10,62 @@ set -e
 
 git submodule update --init --recursive
 
-source set_build_paths.sh
+# source set_build_paths.sh
 
-echo "${BLUE}== bc-crypto-base ==${RESET}"
+# echo "${BLUE}== bc-crypto-base ==${RESET}"
 
-pushd deps/bc-crypto-base
-./configure --prefix ${SYSROOT}
-make check
-make install
-popd
+# pushd deps/bc-crypto-base
+# ./configure --prefix ${SYSROOT}
+# make check
+# make install
+# popd
 
-echo "${BLUE}== bc-ur ==${RESET}"
+# echo "${BLUE}== bc-ur ==${RESET}"
 
-pushd deps/bc-ur
-./configure --prefix ${SYSROOT}
-make check
-make install
-popd
+# pushd deps/bc-ur
+# ./configure --prefix ${SYSROOT}
+# make check
+# make install
+# popd
 
 echo "${BLUE}== libwally-core ==${RESET}"
 
 pushd deps/libwally-core
+git submodule init
+git submodule sync --recursive
+git submodule update --init --recursive
 ./tools/autogen.sh
 
 if [[ "$OSTYPE" == "msys" ]]; then
 # FIXME: figure out how to disable PTHREADS instead of tests
 ./configure "${WALLY_CONFIGURE-}" --disable-tests --disable-shared --prefix ${SYSROOT}
 else
-./configure "${WALLY_CONFIGURE-}" --disable-shared --prefix ${SYSROOT}
+# ./configure "${WALLY_CONFIGURE-}" --disable-shared --prefix ${SYSROOT}
+# ./configure "${WALLY_CONFIGURE-}" --disable-shared
+./configure
 fi
 
 make
 make check
-make install
+# make install
 popd
 
-echo "${BLUE}== argp-standalone ==${RESET}"
+# echo "${BLUE}== argp-standalone ==${RESET}"
 
-pushd deps/argp-standalone/argp-standalone
-set +e
-patch -N <../patch-argp-fmtstream.h
-set -e
-./configure --prefix ${SYSROOT}
-make install
-cp libargp.a ${SYSROOT}/lib/
-cp argp.h ${SYSROOT}/include/
-popd
+# pushd deps/argp-standalone/argp-standalone
+# set +e
+# patch -N <../patch-argp-fmtstream.h
+# set -e
+# ./configure --prefix ${SYSROOT}
+# make install
+# cp libargp.a ${SYSROOT}/lib/
+# cp argp.h ${SYSROOT}/include/
+# popd
 
-echo "${BLUE}== keytool ==${RESET}"
+# echo "${BLUE}== keytool ==${RESET}"
 
-./configure
-make clean
-make check
-echo "${GREEN}*** Keytool built.${RESET}"
-echo "${GREEN}Next step: sudo make install${RESET}"
+# ./configure
+# make clean
+# make check
+# echo "${GREEN}*** Keytool built.${RESET}"
+# echo "${GREEN}Next step: sudo make install${RESET}"
